1652e854b33a9106adb17afcdfe5c0ededcd2344
Jack Beringen <jack.ber.private@gmail.com>
2021-05-15 00:05:50 +1000

    Revert "cors test"
    This reverts commit 98e044edf0186d80b5b4ab086e8c490053e52e51.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-  "Access-Control-Max-Age": "86400",
-};
-
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
-});
+})
 
 /**
  * Respond with hello worker text
@@ -15,12 +9,6 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const url = new URL(request.url);
 
-  // Call the API
-  const response = await fetch(`https://apix.facepunch.com/api/sbox${url.pathname}${url.search}`, request);
-
-  // Set the CORS header
-  response.headers.set('Access-Control-Allow-Origin', '*');
-
   // Return the JSON api response
-  return response;
+  return await fetch(`https://apix.facepunch.com/api/sbox${url.pathname}${url.search}`, request);
 }
